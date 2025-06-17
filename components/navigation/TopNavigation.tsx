3da9e25ffe199356f923aac27769c298fbe5acd6
lhjwork <jin.come.up.business@gmail.com>
2025-06-18 00:35:42 +0900

    first commit

---
+++ components/navigation/TopNavigation.tsx
@@ -0,0 +1,8 @@
+export default function TopNavigation() {
+  return (
+    <nav className="flex flex-col w-full rounded-b-xl border-2">
+      <div>주소</div>
+      <div>Search bar</div>
+    </nav>
+  );
+}
