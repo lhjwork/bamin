d3ff509063507d3b36b33acb16969aac30be8f42
lhjwork <jin.come.up.business@gmail.com>
2025-10-22 17:22:19 +0900

    component create bottom navi Main section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
+import BottomNavigation from "@/components/navigation/BottomNavigation";
+import TopNavigation from "@/components/navigation/TopNavigation";
+import MainSection from "@/components/section/MainSection";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>hellow world</div>;
+  return (
+    <main>
+      <TopNavigation />
+      <MainSection />
+      <BottomNavigation />
+    </main>
+  );
 }
---
+++ components/navigation/TopNavigation.tsx
@@ -0,0 +1,10 @@
+export default function TopNavigation() {
+  return (
+    <nav className="">
+      <div className="flex flex-col h-20 w-full justify-center items-center border rounded-b-xl bg-teal-300/80">
+        <div>Address</div>
+        <div>Search bar</div>
+      </div>
+    </nav>
+  );
+}
